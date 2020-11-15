6e745eca5024888167771ffb2372d9ea4a75651a
Peter Tan <alucarrd@gmail.com>
2020-11-15 13:41:28 -0800

    Added wind direction and deleted humidity

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -8,7 +8,8 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.current.weather_descriptions+ '. It is currently ' + body.current.temperature + ' degress out. It feels like ' + body.current.feelslike+ ' degrees. The humidity  is ' + body.current.humidity + '.')
+            console.log(body.current)
+            callback(undefined, body.current.weather_descriptions+ '. It is currently ' + body.current.temperature + ' degress out. It feels like ' + body.current.feelslike+ ' degrees. The wird direction is ' + body.current.wind_dir + '.')
         }
     })
 }
